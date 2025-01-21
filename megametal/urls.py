d4dbe0469aa75f628eb7284577449683eb19ca97
anointing adebayo <124325944+anointing1234@users.noreply.github.com>
2025-01-21 21:24:13 +0100

    commit

--- megametal/urls.py
+++ megametal/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve 
 from django.conf.urls import handler404, handler500
+from accounts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
